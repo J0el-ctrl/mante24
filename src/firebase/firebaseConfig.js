3ca69fcec210943c6f0885fedeb29ee112accf69
joelBarerra <joelbarreraarotoma@gmail.com>
2024-07-10 17:34:13 -0500

    crud-firebase-9

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -17,17 +17,17 @@ export default db;
 
 
 // cuando se crea con cra y no en vite no funka
-// apiKey:process.env.REACT_APP_FIREBASE_API_KEY,
-// authDomain:process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-// projectId:process.env.REACT_APP_FIREBASE_PROJECT_ID,
-// storageBucket:process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-// messagingSenderId:process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-// appId:process.env.REACT_APP_FIREBASE_APP_ID,
+// apiKey: import.meta.env.VITE_APP_APIKEY, 
+// authDomain: import.meta.env.VITE_APP_AUTHDOMAIN,
+// projectId: import.meta.env.VITE_APP_PROJECTID,
+// storageBucket: import.meta.env.VITE_APP_STORAGEBUCKET,
+// messagingSenderId: import.meta.env.VITE_APP_MESSAGINGSENDERID,
+// appId: import.meta.env.VITE_APP_APPID
 
 // en global archivo  .env.local
-// REACT_APP_FIREBASE_API_KEY=AIzaSyCLBRE1gX409ZA7g9s4IJVO267n4gN8cIk
-// REACT_APP_FIREBASE_AUTH_DOMAIN=maintenance-fire9.firebaseapp.com
-// REACT_APP_FIREBASE_PROJECT_ID=maintenance-fire9
-// REACT_APP_FIREBASE_STORAGE_BUCKET=maintenance-fire9.appspot.com
-// REACT_APP_FIREBASE_MESSAGING_SENDER_ID=771581955941
-// REACT_APP_FIREBASE_APP_ID=1:771581955941:web:f77f0ffd1c9c48deac9b8c"
+// VITE_APP_APIKEY=
+// VITE_APP_AUTHDOMAIN=
+// VITE_APP_PROJECTID=
+// VITE_APP_STORAGEBUCKET=
+// VITE_APP_MESSAGINGSENDERID=
+// VITE_APP_APPID=
